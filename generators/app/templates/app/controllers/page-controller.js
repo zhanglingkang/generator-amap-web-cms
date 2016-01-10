384cf665c18ba28530bd9be5ef2682b7c6381abd
张凌康 <lingkang.zlk@alibaba-inc.com>
2016-01-10 09:06:08 +0800

    init

--- generators/app/templates/app/controllers/page-controller.js
+++ generators/app/templates/app/controllers/page-controller.js
@@ -1,6 +1,5 @@
 var route = require('koa-route')
 app.use(function*(next) {
-    console.log(this.path.substring(1))
     if (/^\//.test(this.path)) {
         yield this.render(this.path.substring(1))
     }
